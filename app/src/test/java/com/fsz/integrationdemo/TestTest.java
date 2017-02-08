6db20a282cadfd1d807900239f4330e6532e7fa0
fsz <515697187@qq.com>
2017-02-08 22:22:01 +0800

    java
    exit
    quit

---
+++ app/src/test/java/com/fsz/integrationdemo/TestTest.java
@@ -0,0 +1,21 @@
+package com.fsz.integrationdemo;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fsz on 2017/2/8.
+ */
+public class TestTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void aaa() throws Exception {
+
+    }
+
+}
