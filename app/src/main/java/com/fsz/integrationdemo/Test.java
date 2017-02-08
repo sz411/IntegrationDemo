e3d59cf9c8fd119934e6da2238865b154d67538e
fsz <515697187@qq.com>
2017-02-08 22:27:37 +0800

    git

--- app/src/main/java/com/fsz/integrationdemo/Test.java
+++ app/src/main/java/com/fsz/integrationdemo/Test.java
@@ -1,9 +1,18 @@
 package com.fsz.integrationdemo;
 
+import android.util.Log;
+
 /**
  * Created by fsz on 2017/2/8.
  */
 
 public class Test {
+
+    int aaa(){
+        int a = 4;
+        int[] b = {};
+        a = b[2];
+       return a++;
+    }
 
 }
--- app/src/test/java/com/fsz/integrationdemo/ExampleUnitTest.java
+++ app/src/test/java/com/fsz/integrationdemo/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.fsz.integrationdemo;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,4 +16,5 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
 }
