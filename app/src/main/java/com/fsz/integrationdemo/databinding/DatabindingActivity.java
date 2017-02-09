f212588bdf8650ba4d60e2bf69f2079a8e69ca34
fsz <515697187@qq.com>
2017-02-09 10:00:22 +0800

    删除测试，增加databinding

---
+++ app/src/main/java/com/fsz/integrationdemo/databinding/DatabindingActivity.java
@@ -0,0 +1,22 @@
+package com.fsz.integrationdemo.databinding;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fsz.integrationdemo.R;
+
+/**
+ * Created by fsz on 2017/2/9.
+ */
+
+public class DatabindingActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_databinding);
+
+    }
+
+}
---
+++ app/src/main/java/com/fsz/integrationdemo/databinding/user.java
@@ -0,0 +1,11 @@
+package com.fsz.integrationdemo.databinding;
+
+/**
+ * Created by fsz on 2017/2/9.
+ */
+
+public class User {
+
+    public String test = "zheshi yige ceshi";
+
+}
